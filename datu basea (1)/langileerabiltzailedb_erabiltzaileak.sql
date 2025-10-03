6ec9b6729aad0c99baf21db0924c01a42437da82
Aitor Auzmendi <aitorauzmen@gmail.com>
2025-10-03 16:52:14 +0200

    snake case bezela

--- datu basea (1)/langileerabiltzailedb_erabiltzaileak.sql
+++ datu basea (1)/langileerabiltzailedb_erabiltzaileak.sql
@@ -16,32 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `ekintzak`
+-- Table structure for table `erabiltzaileak`
 --
 
-DROP TABLE IF EXISTS `ekintzak`;
+DROP TABLE IF EXISTS `erabiltzaileak`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ekintzak` (
+CREATE TABLE `erabiltzaileak` (
   `Id` int NOT NULL AUTO_INCREMENT,
-  `ErabiltzaileId` int DEFAULT NULL,
-  `EkintzaMota` enum('SORTU','ALDATU','EZABATU') NOT NULL,
-  `TaulaIzena` varchar(50) NOT NULL,
-  `ErregistroId` int NOT NULL,
-  `DataEkintza` datetime DEFAULT CURRENT_TIMESTAMP,
+  `LangileId` int NOT NULL,
+  `ErabiltzaileIzena` varchar(50) NOT NULL,
+  `Pasahitza` varchar(255) NOT NULL,
+  `Ezabatuta` tinyint(1) DEFAULT '0',
+  `DataSortu` datetime DEFAULT CURRENT_TIMESTAMP,
+  `DataEzabatu` datetime DEFAULT NULL,
   PRIMARY KEY (`Id`),
-  KEY `ErabiltzaileId` (`ErabiltzaileId`),
-  CONSTRAINT `ekintzak_ibfk_1` FOREIGN KEY (`ErabiltzaileId`) REFERENCES `erabiltzaileak` (`Id`)
+  UNIQUE KEY `ErabiltzaileIzena` (`ErabiltzaileIzena`),
+  KEY `LangileId` (`LangileId`),
+  CONSTRAINT `erabiltzaileak_ibfk_1` FOREIGN KEY (`LangileId`) REFERENCES `langileak` (`Id`) ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ekintzak`
+-- Dumping data for table `erabiltzaileak`
 --
 
-LOCK TABLES `ekintzak` WRITE;
-/*!40000 ALTER TABLE `ekintzak` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ekintzak` ENABLE KEYS */;
+LOCK TABLES `erabiltzaileak` WRITE;
+/*!40000 ALTER TABLE `erabiltzaileak` DISABLE KEYS */;
+/*!40000 ALTER TABLE `erabiltzaileak` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -53,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-03 16:48:58
+-- Dump completed on 2025-10-03 16:51:49
